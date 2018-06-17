34f4f6acdbc9d145e0351bfb34c627e5bf2d3dae
zawhtetnaing10 <zawgyi.gog@gmail.com>
2018-06-17 16:21:10 +0630

    Please Fetch and Pull. U can do it. I love u

--- src/main/java/com/pesandar/schoolmanagement/CMmain.java
+++ src/main/java/com/pesandar/schoolmanagement/CMmain.java
@@ -10,17 +10,18 @@ public class CMmain {
 
 	public static void main(String[] args) {
 		SchoolManagementDao sdao1=new SchoolManagementDao();
-		sdao1.getAllSchools().stream().map(SchoolManagement::getSchoolfounder).forEach(System.out::println);
+		
 		SchoolManagement schoolmanagement1=new SchoolManagement();
-		schoolmanagement1.setId(7);
+		schoolmanagement1.setId(2);
 		schoolmanagement1.setSchoolfounder("PE SanDar");
 		schoolmanagement1.setSchoolname("Winner");
 		schoolmanagement1.setSchooladdress("Shwe Taung Kyar");
 		schoolmanagement1.setSchoolphno("09786820265");
 		
 		
-		SchoolManagementDao sdao2=new SchoolManagementDao();
-		sdao2.insertNextData(schoolmanagement1);
+		
+		sdao1.insertNextData(schoolmanagement1);
+		sdao1.getAllSchools().stream().map(SchoolManagement::getSchoolfounder).forEach(System.out::println);
 
 	}
 
--- src/main/java/com/pesandar/schoolmanagement/model/dao/SchoolManagementDao.java
+++ src/main/java/com/pesandar/schoolmanagement/model/dao/SchoolManagementDao.java
@@ -12,44 +12,47 @@ import com.pesandar.schoolmanagement.ConnectionManager;
 import com.pesandar.schoolmanagement.model.entities.SchoolManagement;
 
 public class SchoolManagementDao {
-	public List<SchoolManagement> getAllSchools(){
-		String sql="select * from schoolmanagement";
-		List<SchoolManagement> schoolList=new ArrayList<>();
-		try(Connection connection=ConnectionManager.getInstance();
-				Statement statement=connection.createStatement();){
-			ResultSet rs=statement.executeQuery(sql);
+	public List<SchoolManagement> getAllSchools() {
+		String sql = "select * from schoolmanagement";
+		List<SchoolManagement> schoolList = new ArrayList<>();
+		try (Connection connection = ConnectionManager.getInstance();
+				Statement statement = connection.createStatement();) {
+			ResultSet rs = statement.executeQuery(sql);
 			while (rs.next()) {
-				String schoolfounder=rs.getString("schoolfounder");
-				String schoolname=rs.getString("schoolname");
-				String schooladdress=rs.getString("schooladdress");
-				String schoolphno=rs.getString("schoolphno");
-				SchoolManagement schoolmanagement=new SchoolManagement();
+				String schoolfounder = rs.getString("schoolfounder");
+				String schoolname = rs.getString("schoolname");
+				String schooladdress = rs.getString("schooladdress");
+				String schoolphno = rs.getString("schoolphno");
+				SchoolManagement schoolmanagement = new SchoolManagement();
 				schoolmanagement.setSchoolfounder(schoolfounder);
 				schoolmanagement.setSchoolname(schoolname);
 				schoolmanagement.setSchooladdress(schooladdress);
 				schoolmanagement.setSchoolphno(schoolphno);
 				schoolList.add(schoolmanagement);
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}return schoolList;
-		
+		}
+		return schoolList;
+
 	}
+
 	public void insertNextData(SchoolManagement schoolmanagement1) {
-		String sql="insert into schoolmanagement values(?,?,?,?,?)";
-		try(Connection connection=ConnectionManager.getInstance();
-			PreparedStatement prestatement=connection.prepareStatement(sql)) {
-			prestatement.setInt(1,schoolmanagement1.getId());
-		prestatement.setString(2,schoolmanagement1.getSchoolname());
-		prestatement.setString(3,schoolmanagement1.getSchoolfounder());
-		prestatement.setString(4,schoolmanagement1.getSchooladdress());
-		prestatement.setString(5,schoolmanagement1.getSchoolphno());
-		
-		prestatement.executeUpdate(sql);
-		
+		String sql = "insert into schoolmanagement values(?,?,?,?,?)";
+		try (Connection connection = ConnectionManager.getInstance();
+				PreparedStatement prestatement = connection.prepareStatement(sql)) {
+			
+			prestatement.setInt(1, schoolmanagement1.getId());
+			prestatement.setString(2, schoolmanagement1.getSchoolname());
+			prestatement.setString(3, schoolmanagement1.getSchoolfounder());
+			prestatement.setString(4, schoolmanagement1.getSchooladdress());
+			prestatement.setString(5, schoolmanagement1.getSchoolphno());
+
+			prestatement.executeUpdate();
+
 		} catch (Exception e) {
-				e.printStackTrace();
-			}
+			e.printStackTrace();
+		}
 	}
 }
